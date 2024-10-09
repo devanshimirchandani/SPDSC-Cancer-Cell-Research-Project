6f8a266adcd3b08bd3dbc8986f45bd8b40a79133
Veronicazwt <wantungtsou@gmail.com>
2024-10-10 00:48:39 +1100

    Update Data Processing & Visual.R

--- Data Processing & Visual.R
+++ Data Processing & Visual.R
@@ -1,59 +1,73 @@
-library(Seurat)
 library(dplyr)
+library(Seurat)
+library(patchwork)
 library(ggplot2)
-
 
 gene_data <- CrC_counts
 metadata <- CrC_metadata
 
-# Check orig.ident
-unique(CrC_metadata$orig.ident)
+unique(CrC_metadata$Donor)
 
-crc_seurat[["percent.mt"]] <- PercentageFeatureSet(crc_seurat, pattern = "^MT-")
+crc_seurat <- CreateSeuratObject(counts = CrC_counts)
+crc_seurat <- AddMetaData(object = crc_seurat, metadata = as.data.frame(CrC_metadata))
 
-CrC_metadata_subset <- subset(CrC_metadata, orig.ident %in% c("B001-A-301", "F007", "CRC1_8810"))
-
-# Subset the counts data based on the cells in the filtered metadata
-CrC_counts_subset <- CrC_counts[, rownames(CrC_metadata_subset)]
-
-# Create the Seurat object with the subset data
-crc_seurat <- CreateSeuratObject(counts = CrC_counts_subset, meta.data = CrC_metadata_subset, project = "CRC_analysis", min.cells = 3, min.features = 200)
+# View the basic structure of the Seurat object
 crc_seurat
 
+length(Cells(crc_seurat))
+length(crc_seurat@meta.data$Donor)
 
-VlnPlot(crc_seurat, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3) 
+table(crc_seurat@meta.data$Donor)
 
-crc_seurat <- subset(crc_seurat, subset = nFeature_RNA > 200 & percent.mt < 5)
+cells_by_donor <- split(Cells(crc_seurat), crc_seurat@meta.data$Donor)
 
-# Normalisation
-crc_seurat <- NormalizeData(crc_seurat)
+set.seed(123)  # Set seed for reproducibility 
+sampled_cells <- unlist(lapply(cells_by_donor, function(cells) { sample(cells, size = min(5000, length(cells)), replace = FALSE)}))
 
-# Feature Scatter plots
-plot1 <- FeatureScatter(crc_seurat, feature1 = "nCount_RNA", feature2 = "percent.mt")
-plot2 <- FeatureScatter(crc_seurat, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
-plot1 + plot2
+sampled_crc_seurat <- subset(crc_seurat, cells = sampled_cells)
+sampled_crc_seurat
 
-crc_seurat <- FindVariableFeatures(crc_seurat, selection.method = "vst", nfeatures = 2000)
+sampled_crc_seurat[["percent.mt"]] <- PercentageFeatureSet(sampled_crc_seurat, pattern = "^MT-")
+
+VlnPlot(sampled_crc_seurat, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3) 
+
+sampled_crc_seurat <- subset(sampled_crc_seurat, subset = nFeature_RNA > 200 & percent.mt < 5)
+
+sampled_crc_seurat <- NormalizeData(sampled_crc_seurat)
+
+plot1 <- FeatureScatter(sampled_crc_seurat, feature1 = "nCount_RNA", feature2 = "percent.mt", group.by = "Donor")
+plot2 <- FeatureScatter(sampled_crc_seurat, feature1 = "nCount_RNA", feature2 = "nFeature_RNA", group.by = "Donor")
+
+plot1
+plot2
+
+
+sampled_crc_seurat <- FindVariableFeatures(sampled_crc_seurat, selection.method = "vst", nfeatures = 2000)
 
 # Identify the 10 most highly variable genes
-top10 <- head(VariableFeatures(crc_seurat), 10)
+top10 <- head(VariableFeatures(sampled_crc_seurat), 10)
 
 # Plot variable features with and without labels
-plot_a <- VariableFeaturePlot(crc_seurat)
-plot_b <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
-plot_a + plot_b
+plot1 <- VariableFeaturePlot(sampled_crc_seurat)
+plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
 
-# Scaling data
-all.genes <- rownames(crc_seurat)
-crc_seurat <- ScaleData(crc_seurat, features = all.genes)
+plot1
+plot2
 
-## PCA
-crc_seurat <- RunPCA(crc_seurat, features = VariableFeatures(crc_seurat))
-VizDimLoadings(crc_seurat, dims = 1:2, reduction = "pca") 
-DimPlot(crc_seurat, reduction = "pca")
+all.genes <- rownames(sampled_crc_seurat)
+sampled_crc_seurat <- ScaleData(sampled_crc_seurat, features = all.genes)
 
-## UMAP
-crc_seurat <- RunUMAP(crc_seurat, dims = 1:10) 
-DimPlot(crc_seurat, reduction = "umap")
+sampled_crc_seurat
+
+sampled_crc_seurat <- RunPCA(sampled_crc_seurat, features = VariableFeatures(sampled_crc_seurat))
+
+VizDimLoadings(sampled_crc_seurat, dims = 1:2, reduction = "pca") 
+
+DimPlot(sampled_crc_seurat, reduction = "pca", group.by = "Donor")
+
+sampled_crc_seurat <- RunUMAP(sampled_crc_seurat, dims = 1:10) 
+
+DimPlot(sampled_crc_seurat, reduction = "umap", group.by = "Donor")
+
 
 
